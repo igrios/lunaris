c475012db213966e364361d6ccb62f757f82f62e
Ignacio Rios <ignarios1@hotmail.com>
2026-07-26 14:29:18 -0300

    fix: target accounts table for password reset

--- src/main/resources/db/migration/V53__reset_admin_password.sql
+++ src/main/resources/db/migration/V53__reset_admin_password.sql
@@ -1,3 +1,4 @@
 UPDATE accounts
-SET password_hash = '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQvq4a.'
+SET password_hash = '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.AQvq4a.',
+    active = true
 WHERE upper(username) = upper('ignacio');
